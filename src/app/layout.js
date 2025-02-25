3bd0830917a57a32e76bd3f75bdeee084f914671
kevinlampe1998 <lampekevin@outlook.com>
2025-02-25 09:30:52 +0100

    added context-provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import ContextProvider from "@/components/context-provider/context-provider";
+import Header from "@/components/header/header";
+// import Header2 from "@/components/header2/header2";
+// import SlideBar from "@/components/slidebar/slidebar";
+// import ForeGround from "@/components/foreground/foreground";
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <ContextProvider>
+        <body>
+          {/* <ForeGround/> */}
+          <Header/>
+          {/* <Header2/> */}
+          {/* <SlideBar/> */}
+          {/* <main>
+            {children}
+          </main> */}
+        </body>
+      </ContextProvider>
     </html>
   );
 }
