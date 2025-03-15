ce01b5f54951a17a5b5045ef9b73260a787c760f
kevinlampe1998 <lampekevin@outlook.com>
2025-03-15 18:13:55 +0100

    added https:// in base url

--- src/lib/base-url.js
+++ src/lib/base-url.js
@@ -1,5 +1,5 @@
 const BASE_URL =
     process.env.NODE_ENV === 'production' ?
-        'youtube-clone-2-nextjs-mongodb-cloudinary.vercel.app' : '';
+        'https://youtube-clone-2-nextjs-mongodb-cloudinary.vercel.app' : '';
 
 export default BASE_URL;
